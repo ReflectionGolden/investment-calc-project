4774ef474a65210dcf6501657774d1a86bc66cef
Golden_Reflection <reflectiongolden@gmail.com>
2026-01-26 11:31:01 +0000

    removed unneeded fragments from UserInput

--- src/components/UserInput.jsx
+++ src/components/UserInput.jsx
@@ -4,7 +4,6 @@ import '../App.css'
 const UserInput = ({userInput, handleInputChange, resetValue}) => {
 
   return(
-    <>
     <section className='p-4 max-w-200 my-8 mx-auto rounded-sm bg-gray-900'>
       <form>
         <div className='flex justify-between gap-6 mb-2'>
@@ -18,25 +17,22 @@ const UserInput = ({userInput, handleInputChange, resetValue}) => {
         </div>
       </form>
     </section>
-    </>
   ) 
 }
 
 function InvestInput(props) {
   return(
-    <>
-      <p>
-        <label className='font-bold' htmlFor={props.id}>{props.label}</label>
-        <input 
-          className='w-40 p-2 border border-stone-300 rounded-sm text-base'
-          type="number"
-          id={props.id}
-          value={props.value}
-          onChange={(e) => props.handleInputChange(props.id, e.target.value)}
-          min={1}
-        />
-      </p>
-    </>
+    <p>
+      <label className='font-bold' htmlFor={props.id}>{props.label}</label>
+      <input 
+        className='w-40 p-2 border border-stone-300 rounded-sm text-base'
+        type="number"
+        id={props.id}
+        value={props.value}
+        onChange={(e) => props.handleInputChange(props.id, e.target.value)}
+        min={1}
+      />
+    </p>
   )
 }
 
